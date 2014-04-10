3a3855681c7735634b8556ff9a709567f431242b
Dustin Quam <quamsta@gmail.com>
2014-04-10 10:33:38 -0500

    fixed for local env + composer and SS update

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,31 +3,22 @@
 global $project;
 $project = 'mysite';
 
-global $databaseConfig;
-$databaseConfig = array(
-	"type" => 'MySQLDatabase',
-	"server" => 'localhost',
-	"username" => 'root',
-	"password" => 'root',
-	"database" => 'scope',
-	"path" => '',
-);
+global $database;
+$database = 'scope';
+ 
+// Use _ss_environment.php file for configuration
+require_once("conf/ConfigureFromEnv.php");
 
 MySQLDatabase::set_connection_charset('utf8');
 
-// Set the current theme. More themes can be downloaded from
-// http://www.silverstripe.org/themes/
-SSViewer::set_theme('scope-redesign');
-
+// This line set's the current theme. More themes can be
+// downloaded from http://www.silverstripe.org/themes/
+SSViewer::set_theme('scope');
 // Set the site locale
 i18n::set_locale('en_US');
 
-// Enable nested URLs for this site (e.g. page/sub-page/)
-if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
-
-Director::set_environment_type('dev');
-
-
-FulltextSearchable::enable();
-
-
+// enable nested URLs for this site (e.g. page/sub-page/)
+SiteTree::enable_nested_urls();
+/* Paste as Plain Text By Default */
+HtmlEditorConfig::get('cms')->setOption('paste_text_sticky',true);
+HtmlEditorConfig::get('cms')->setOption('paste_text_sticky_default',true);
